93e6a76b51081bb7bb2ff3459d83e90c41617cf7
rishitaagarwal82-oss <rishitaagarwal82@gmail.com>
2026-07-06 00:17:54 -0700

    fix production API URLs

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://fivescore-fullstack-mvp-2.onrender.com",
 });
 
 export default API;
