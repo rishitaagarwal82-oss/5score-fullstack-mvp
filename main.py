ca4c4940e09dedc004a4a5cb2c1fc04787e6aa0d
rishitaagarwal82-oss <rishitaagarwal82@gmail.com>
2026-07-17 14:23:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from quiz import router as quiz_router
+from frq import router as frq_router
+
+print("🔥 MAIN.PY IS RUNNING")
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(quiz_router)
+app.include_router(frq_router)
+
+
 @app.get("/")
-def root():
-    return {"status": "ok"}
+def home():
+    return {"message": "Quiz backend is running"}
